f257a16840d4057e6160f87aeff7cc1a048d22e2
Deepakkumareswaran <37336372+Deepakkumareswaran@users.noreply.github.com>
2018-03-20 15:24:11 +0530

    Update Find given num odd or even.py

--- Beginner level/Find given num odd or even.py
+++ Beginner level/Find given num odd or even.py
@@ -1,6 +1,5 @@
-while(1):
-    a = int(input("ENTER THE NUMBER : "))
-    if a%2==0:
-        print(a,"IS EVEN")
-    else:
-        print(a,"IS ODD")
+a = int(input("ENTER THE NUMBER : "))
+if a%2==0:
+    print(a,"IS EVEN")
+else:
+    print(a,"IS ODD")
