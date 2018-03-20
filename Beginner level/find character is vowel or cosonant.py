368841aac6b6a4a6bef83a439a07a6a40034d94b
Deepakkumareswaran <37336372+Deepakkumareswaran@users.noreply.github.com>
2018-03-20 15:43:26 +0530

    Create find character is vowel or cosonant.py

---
+++ Beginner level/find character is vowel or cosonant.py
@@ -0,0 +1,5 @@
+w=input("enter the character : ")
+if ((w=='a' or 'A') or (w=='e' or 'E') or (w=='i' or 'I') or (w=='o' or 'O') or (w=='u' or 'U')):
+    print("THE CHARACTER IS VOWEL")
+else:
+     print("THE CHARACTER IS CONSONANT")
