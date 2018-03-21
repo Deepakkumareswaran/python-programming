63bd5e7619cb1d6f179049c8aa750cdc5de77b8e
Deepakkumareswaran <37336372+Deepakkumareswaran@users.noreply.github.com>
2018-03-21 12:43:44 +0530

    Create rolling dice.py

---
+++ Beginner level/rolling dice.py
@@ -0,0 +1,17 @@
+import random
+flag = 1
+min = 1
+max = 6
+input("press enter to  roll ")
+while flag==1:
+    e=random.randint(min,max)
+    print(e)
+    if(e==6 or e==1):
+        print("you have another turn")
+        input("press enter to roll the diec again..!")
+        f=random.randint(min,max)
+        print(f)
+    else:
+        print("your turn over")
+        print ("Try Again...!")
+        flag = 0
